67d4a141088d142a52ccf7ab9ade0c6644d0875d
Mondragon <fimv705@gmail.com>
2024-06-04 22:49:50 -0600

    Finally added comments for better understanding

--- CheckerboardFill.py
+++ CheckerboardFill.py
@@ -8,11 +8,13 @@ def cover(board, lab=1, top=0, left=0, side=None):
 
     for dy_outer, dy_inner in offsets:
         for dx_outer, dx_inner in offsets:
-            if not board[top+dy_outer][left+dx_outer]:
-                board[top+s+dy_inner][left+s+dx_inner] = lab
+            # print(top + dy_outer, left + dx_outer)
+            if not board[top+dy_outer][left+dx_outer]:          # Searches for the missing CORNER
+                # print('Not found', lab)
+                board[top+s+dy_inner][left+s+dx_inner] = lab    # Sets every center piece except the one in the cuadrant of the missing corner
 
     lab += 1
-    if s > 1:
+    if s > 1:                                                   # Divides the board in subboards of min length = 2
         for dy in [0, s]:
             for dx in [0, s]:
                 lab = cover(board, lab, top+dy, left+dx, s)
@@ -20,7 +22,9 @@ def cover(board, lab=1, top=0, left=0, side=None):
 
 
 board = [[0]*8 for i in range(8)]
-board[4][5] = -1
+board[0][0] = -1
+
 cover(board)
+
 for row in board:
-    print((" %2i"*8)% tuple(row))
+    print((" %2i"*8) % tuple(row))                              # 2i: The number of min digits. The number of times the string is repeated must be the same as the len of the tuple
